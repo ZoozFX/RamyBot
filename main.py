cc036040e44ad2b16ebe6f871d077d37bddf76c5
ZoozFX <145346039+ZoozFX@users.noreply.github.com>
2025-07-18 12:30:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/ar", response_class=HTMLResponse)
-async def form_ar(request: Request):
+@app.get("/form_ar.html", response_class=HTMLResponse)
+async def serve_form_ar(request: Request):
     return templates.TemplateResponse("form_ar.html", {"request": request})
 
-@app.get("/en", response_class=HTMLResponse)
-async def form_en(request: Request):
+@app.get("/form_en.html", response_class=HTMLResponse)
+async def serve_form_en(request: Request):
     return templates.TemplateResponse("form_en.html", {"request": request})
-
-@app.post("/register")
-async def register(request: Request):
-    data = await request.json()
-    print("Received data:", data)  # يمكنك استبدال هذا بإرسال البيانات إلى البوت
-    return {"status": "success"}
