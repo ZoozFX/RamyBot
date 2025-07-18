d7d697d02f28b416d382864102b5cdf24486f985
ZoozFX <145346039+ZoozFX@users.noreply.github.com>
2025-07-18 12:46:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
+#from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
